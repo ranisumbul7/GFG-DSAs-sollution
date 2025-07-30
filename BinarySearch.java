72febcadab53d86d551adf5903a9ac50eb80aca2
ranisumbul7 <ranisumbul7@gmail.com>
2025-07-30 22:43:55 +0530

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int binarysearch(int[] arr, int k) {
+        // Code Here
+        int index=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==k)
+            {
+                index=i;
+                break;
+            }
+            else
+            {
+                index=-1;
+            }
+        }
+        
+        return index;
+    }
+}
